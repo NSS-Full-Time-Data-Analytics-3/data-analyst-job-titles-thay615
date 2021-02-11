50a46810cf5ce16faa6cecedd4af3816a307dc16
thay615 <thay615@gmail.com>
2021-02-11 08:59:15 -0600

    revise

--- scripts/project1.sql
+++ scripts/project1.sql
@@ -27,11 +27,12 @@ from data_analyst_jobs
 where review_count between 500 and 1000; 
 --#5: 151
 
-select company, location as state, star_rating as avg_rating
+select location as state, avg(star_rating) as avg_rating
 from data_analyst_jobs
-where star_rating is not null
+where star_rating notnull
+group by location
 order by avg_rating desc;
---#6 GA...........
+--#6 NE
 
 select count(distinct title)
 from data_analyst_jobs;
@@ -42,27 +43,32 @@ from data_analyst_jobs
 where location = 'CA';
 --#8 230
 
-select count (*)
-from data_analyst_jobs
-where review_count > 5000;
---#9 185
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > '5000'
+AND company IS NOT NULL
+GROUP BY company;
+--#9 40
 
-select *
+select company, avg(star_rating) as ratings
 from data_analyst_jobs
 where review_count > 5000
-order by star_rating desc;
---#10 4.199
+and company is not null
+and star_rating is not null
+group by company
+order by ratings desc;
+--#10 
 
-select count (*)
+select count (distinct title)
 from data_analyst_jobs
-where title like 'Analyst%';
---#11 12
+where title like '%Analyst%';
+--#11 774, 754 ilike and like
 
-select title
+select distinct(title)
 from data_analyst_jobs
-where title = 'Analyst%'
-AND title = 'Analytics';
---#12 0
+where title not iLIKE '%Analyst%'
+and title not iLIKE '%Analytics%';
+--#12 4 and Tableau
 
 select *
 from data_analyst_jobs
@@ -72,3 +78,13 @@ order by domain desc
 limit domain 4;
 --Transport and Freight, Telecommunications, Retails
 
+SELECT DISTINCT(domain) AS industry,
+	COUNT(title) AS num_of_hard_to_fill_jobs,
+	MAX(skill) AS skill,
+	AVG(days_since_posting) AS days_since_posting
+FROM data_analyst_jobs
+WHERE domain IS NOT NULL
+AND skill LIKE '%SQL%'
+AND days_since_posting > 21
+GROUP BY domain
+ORDER BY num_of_hard_to_fill_jobs DESC;
